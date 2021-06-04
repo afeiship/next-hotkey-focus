7a3fe19c6f01d610e0fc2fe66471f6ba171ad16e
afeiship <1290657123@qq.com>
2021-06-04 21:18:55 +0800

    fix: initlize rmed

--- src/index.js
+++ src/index.js
@@ -4,21 +4,17 @@
   var nx = global.nx || require('@jswork/next');
   var hotkeys = global.hotkeys || require('hotkeys-js');
   var defaults = { keys: [], selector: 'input[data-hotkey-focus]', callback: nx.noop };
-  var initialized = false;
 
   nx.hotkeyFocus = function (inOptions) {
     var options = nx.mix(null, defaults, inOptions);
-    if (!initialized) {
-      initialized = true;
-      var keys = options.keys.join();
-      var els = nx.slice(document.querySelectorAll(options.selector));
-      hotkeys(keys, function (event, handler) {
-        els.forEach(function (el) {
-          el.focus();
-          options.callback({ event: event, handler: handler });
-        });
+    var keys = options.keys.join();
+    var els = nx.slice(document.querySelectorAll(options.selector));
+    hotkeys(keys, function (event, handler) {
+      els.forEach(function (el) {
+        el.focus();
+        options.callback({ event: event, handler: handler });
       });
-    }
+    });
   };
 
   if (typeof module !== 'undefined' && module.exports) {
