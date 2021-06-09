e76958befc0740305e718211046d6ea3f9e004c3
afeiship <1290657123@qq.com>
2021-06-09 11:10:46 +0800

    fix: el may be nulll + support selectors

--- src/index.js
+++ src/index.js
@@ -2,15 +2,23 @@
   // https://wangchujiang.com/hotkeys/
   var global = typeof window !== 'undefined' ? window : this || Function('return this')();
   var nx = global.nx || require('@jswork/next');
-  var hotkeys = global.hotkeys || require('hotkeys-js');
-  var defaults = { keys: [], selector: 'input[data-hotkey-focus]', callback: nx.noop };
+  var hotkeys = global.hotkeys || require('hotkeys-js').default;
+  var defaults = { keys: [], selectors: ['input[data-hotkey-focus]'], callback: nx.noop };
+
+  var getElement = function (inSelectors) {
+    var elements = inSelectors.reduce(function (res, selector) {
+      var els = nx.slice(document.querySelectorAll(selector));
+      return res.concat(els);
+    }, []);
+    return elements.find(Boolean);
+  };
 
   nx.hotkeyFocus = function (inOptions) {
     var options = nx.mix(null, defaults, inOptions);
     var keys = options.keys.join();
-    var el = document.querySelector(options.selector);
     hotkeys(keys, function (event, handler) {
-      el.focus();
+      var el = getElement(options.selectors);
+      el && el.focus();
       options.callback({ event: event, handler: handler });
     });
   };
